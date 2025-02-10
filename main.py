3dff4f474ed954cd60fe46661519a94b7e189f86
NorIslam04 <islamaoudi18@gmail.com>
2025-02-10 16:59:08 +0100

    add team names

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import requests
 import csv
-import lxml
 from bs4 import BeautifulSoup
 
-date = input('Enter the date in the format of dd/mm/yyyy: ')
+# Demande à l'utilisateur de saisir la date
+date = "2/11/2025"
 
-page = requests.get(f'https://www.yallakora.com/match-center?date={date}')
-soup = BeautifulSoup(page.content, 'lxml')
+# Effectuer la requête HTTP
+url = f'https://www.yallakora.com/match-center?date={date}'
+response = requests.get(url)
 
-matches = soup.find_all('div', class_='matchCard')
-teamsdata = []
-for match in matches:
-    data = match.find_all('div', class_='allData')
-    for d in data:
-        teamsdatas= d.find_all('div', class_='teamsData')
-        teamsdata.extend(teamsdatas)
+# Vérifier si la requête a réussi
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'lxml')
+    
+    # Trouver toutes les cartes de match
+    matches = soup.find_all('div', class_='matchCard')
+    teams=[]
+    
+    for match in matches:
+        data = match.find_all('div', class_='allData')
+        for d in data:
+            teamsdatas= d.find_all('div', class_='teamsData')
+            for t in teamsdatas:
+                team= t.find('p').text
+                teams.append(team)
+
+
     
 
 
 # Write to HTML file
 with open('output.html', 'w', encoding='utf-8') as file:
-    for data in teamsdata:
-        file.write(data.prettify())
+    for data in teams:
+        file.write(data)
         file.write('\n')
